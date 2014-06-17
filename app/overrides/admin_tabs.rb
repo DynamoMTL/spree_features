eb85583ad533925dc973df0d681400cfccf72543
Bryan Mahoney <bryan@goDynamo.com>
2014-06-17 12:55:05 -0400

    Updates icon syntax

--- app/overrides/admin_tabs.rb
+++ app/overrides/admin_tabs.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :name => "features_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:features, :icon => 'icon-tags') %>")
+                     :text => "<%= tab(:features, :icon => 'tags') %>")
 
